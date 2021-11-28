aaf257f878ee04bb6ed548c4a1136790ba973981
april-dev <april20409@hotmail.com>
2021-11-27 22:22:09 -0500

    added sql DDl file

---
+++ DDL.sql
@@ -0,0 +1,18 @@
+create table if not exists schedule.timeSlot
+(
+	Id int not null
+		primary key,
+	Year varchar(4) null,
+	Month varchar(2) null,
+	Day varchar(2) null,
+	StartTime time null,
+	EndTime time null
+);
+
+create table if not exists schedule.availability
+(
+	Id int not null
+		primary key,
+	userId int not null,
+	timeId int not null
+);
