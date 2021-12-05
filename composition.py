f1be0dd3ced4e57de36120f92b53fd1ba7b67edc
Linyu-Li <ll3465@columbia.edu>
2021-12-05 02:02:21 -0500

    add composition.py

--- composition.py
+++ composition.py
@@ -10,80 +10,72 @@ CORS(app)
 # TODO change apis below to AWS ones in deployment
 USR_ADDR_PROPS = {
     'microservice': 'User/address microservice',
-    'api': 'http://localhost:5001/users',
+    'api': 'http://localhost:5001/api/users',
     'fields': ('nameLast', 'nameFirst', 'email', 'addressID', 'password', 'gender')
 }
 USR_PREF_PROPS = {
     'microservice': 'User profile microservice',
-    'api': 'http://localhost:5002/profile',
+    'api': 'http://localhost:5002/api/profile',
     'fields': ('movie', 'hobby', 'book', 'music', 'sport', 'major', 'orientation')
 }
 SCHEDULE_PROPS = {
     'microservice': 'Scheduler microservice',
-    'api': 'http://localhost:5003/availability/users',
-    'fields': ('Year', 'Month', 'Day', 'StartTime', 'EndTime')
+    'api': 'http://localhost:5003/api/availability/users',
+    'fields': 'Id'
 }
-PROPS = (USR_ADDR_PROPS, USR_PREF_PROPS, SCHEDULE_PROPS)
+
 
 
 def project_req_data(req_data: dict, props: tuple) -> dict:
     res = dict()
-    for prop in props:
-        if prop not in req_data:
-            return None
-        res[prop] = req_data[prop]
-    return res
-
+    if type(props) is tuple:
+        for prop in props:
+            if prop not in req_data:
+                return None
+            res[prop] = req_data[prop]
+        return res
+    else:
+        res['Id'] = req_data['Id']
+        return res
 
 def sync_request_microservices(req_data: dict,
                                 headers: Dict) -> (int, str):
-    futures = []
-
     # register users
     register_data = project_req_data(req_data, USR_ADDR_PROPS['fields'])
     if register_data is None:
         return 400, f"Missing data field(s) for {USR_ADDR_PROPS['microservice']}"
-    futures.append(
-        requests.post(USR_ADDR_PROPS['api'],
-                      data=json.dumps(req_data),
-                      headers=headers)
-    )
+    user_res = requests.post(USR_ADDR_PROPS['api'],
+                        data=json.dumps(register_data),
+                        headers=headers)
 
+    if user_res.status_code != 201:
+        return 400, "/user goes wrong, please check"
 
-    # get the uid
-    uid = ''
+    res_str = user_res.json()
+    uid = res_str.split()[4]
 
     # create preference and timeslot
     prop = USR_PREF_PROPS
     data = project_req_data(req_data, prop['fields'])
     if data is None:
         return 400, f"Missing data field(s) for {prop['microservice']}"
-    futures.append(
-        requests.post(prop['api'] + f"/{uid}",
+    data['id'] = uid
+    pref_res = requests.post(prop['api'],
                   data=json.dumps(data),
-                  headers=headers))
+                  headers=headers)
 
+    if pref_res.status_code != 201:
+        return 400, "/profile goes wrong, please check"
 
     prop = SCHEDULE_PROPS
-    for time_slot in req_data['timeSlots']:
-        tid = time_slot['Id']
-        t_data = project_req_data(time_slot, prop['fields'])
-        if t_data is None:
-            return 400, f"Missing field(s) in one of the request data for {prop['microservice']}"
-        futures.append(
-            requests.post(prop['api'] + f"/{uid}/{tid}",
-                      data=json.dumps(data),
-                      headers=headers))
+    data = project_req_data(req_data, prop['fields'])
+    schedule_res = requests.post(prop['api'] + f"/{uid}",
+                  data=json.dumps(data),
+                  headers=headers)
 
+    if schedule_res.status_code != 201:
+        return 400, "/availability goes wrong, please check"
 
-    for i, future in enumerate(futures):
-        microservice = PROPS[min(i, 2)]['microservice']
-        res = future.result()
-        if res is None:
-            return 408, f"{microservice} did not response."
-        elif not res.ok:
-            return res.status_code, \
-                   f"Response from the {microservice} is not OK."
     return 200, "User info created successfully!"
 
 
@@ -96,40 +88,22 @@ def update_info():
     status_code, message = sync_request_microservices(req_data, request.headers)
     return Response(f"{status_code} - {message}", status=status_code, mimetype="application/json")
     """
-        Request JSON format:
+        Request JSON format example:
         {
-            "nameLast": string,
-            "nameFirst": string,
-            "email": string,
-            "addressID": string/int,
-            "password": string,
-            "gender": string,
-            "movie": string,
-            "hobby": string,
-            "book": string,
-            "music": string,
-            "sport": string,
-            "major": string,
-            "orientation": string,
-            "timeSlots": [
-                {
-                    "Id": string/int,
-                    "Year": string,
-                    "Month": string,
-                    "Day": string,
-                    "StartTime": string,
-                    "EndTime": string
-                },
-                {
-                    "Id": string/int,
-                    "Year": string,
-                    "Month": string,
-                    "Day": string,
-                    "StartTime": string,
-                    "EndTime": string
-                },
-                ...
-            ]
+            "nameLast": "Linyu",
+            "nameFirst": "Li",
+            "email": "ll3465@columbia.edu",
+            "addressID":"1",
+            "password": "test",
+            "gender": "test",
+            "movie": "test",
+            "hobby": "test",
+            "book": "test",
+            "music": "test",
+            "sport": "test",
+            "major": "test",
+            "orientation": "test",
+            "Id": "12"
         }
     """
 
